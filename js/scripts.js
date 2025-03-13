cfade46fcf2883ebef3fe3f3a24884e5f261daf8
Mayckon Dantas <mayckonjfd@gmail.com>
2025-03-13 11:24:15 -0300

    world

--- js/scripts.js
+++ js/scripts.js
@@ -55,51 +55,5 @@ window.addEventListener('DOMContentLoaded', event => {
 
 
 
-const estadosAtuacao = ["SP", "SE", "PA", "MG", "CE", "RN", "AL", "RS", "RJ", "MT"];
-const paisesAtuacao = { "Portugal": [-9.14, 38.73], "Uruguai": [-56.16, -34.90], "Michigan": [-85.58, 44.31] };
 
-function desenharMapaBrasil() {
-    fetch("https://raw.githubusercontent.com/codeforamerica/click_that_hood/master/public/data/brazil-states.geojson")
-        .then(res => res.json())
-        .then(data => {
-            const width = 600, height = 600;
-            const projection = d3.geoMercator().fitSize([width, height], data);
-            const path = d3.geoPath().projection(projection);
-
-            const svg = d3.select("#mapa-brasil")
-                .attr("width", width)
-                .attr("height", height);
-
-            svg.selectAll("path")
-                .data(data.features)
-                .enter().append("path")
-                .attr("d", path)
-                .attr("fill", d => estadosAtuacao.includes(d.properties.sigla) ? "#ff5733" : "#ccc")
-                .attr("stroke", "#000");
-        });
-}
-
-function desenharMapaMundo() {
-    fetch("https://raw.githubusercontent.com/deldersveld/topojson/master/world-countries.json")
-        .then(res => res.json())
-        .then(data => {
-            const width = 800, height = 400;
-            const projection = d3.geoMercator().scale(130).translate([width / 2, height / 1.5]);
-            const path = d3.geoPath().projection(projection);
-
-            const svg = d3.select("#mapa-mundo")
-                .attr("width", width)
-                .attr("height", height);
-
-            svg.selectAll("path")
-                .data(topojson.feature(data, data.objects.countries).features)
-                .enter().append("path")
-                .attr("d", path)
-                .attr("fill", d => Object.keys(paisesAtuacao).includes(d.properties.name) ? "#ff5733" : "#ccc")
-                .attr("stroke", "#000");
-        });
-}
-
-desenharMapaBrasil();
-desenharMapaMundo();
 
